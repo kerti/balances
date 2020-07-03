adc583556379e25e8bae85da78ecc790057fd4e1
Raditya Kertiyasa <adit.kerti@gmail.com>
2020-07-03 14:19:05 +0700

    [frontend-web] Pre-format some files for editing.

---
+++ frontend-web/src/views/assets/banks/Banks.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banks = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="card-header">Bank Accounts</div>
+        <div className="card-body">
+          <p>This section will contain all your bank account details.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banks;
