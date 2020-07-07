994e1e3b3c6d234f651432836ac6e3803da6320e
Raditya Kertiyasa <adit.kerti@gmail.com>
2020-07-07 16:53:18 +0700

    Add support for CORS preflight.

--- backend/handler/base.go
+++ backend/handler/base.go
@@ -16,6 +16,9 @@ func respondWithMessage(w http.ResponseWriter, code int, message string) {
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET PUT POST PATCH DELETE OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 	w.WriteHeader(code)
 	w.Write(response)
 }
