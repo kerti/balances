cca9c0abf4050daeddfdf1a89652c433c8ec9fcf
Raditya Kertiyasa <adit.kerti@gmail.com>
2020-07-07 13:24:01 +0700

    Temporarily remove some consts.

--- frontend-web/src/data/actions/actionTypes.js
+++ frontend-web/src/data/actions/actionTypes.js
@@ -1,9 +1,4 @@
 const actionTypes = {
-  auth: {
-    loginSuccess: "AUTH_LOGIN_SUCCESS",
-    loginFailure: "AUTH_LOGIN_FAILURE",
-    logout: "AUTH_LOGOUT",
-  },
   ui: {
     sidebarShow: {
       SET: "UI_SIDEBAR_SHOW_SET",
