18e8ed4d78d2d2a4dca867799262571988edb057
Raditya Kertiyasa <adit.kerti@gmail.com>
2020-07-14 17:33:39 +0700

    Move generic functions outside to generic test files.

--- backend/repository/user_test.go
+++ backend/repository/user_test.go
@@ -7,16 +7,9 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gofrs/uuid"
-	"github.com/jmoiron/sqlx"
-	"github.com/kerti/balances/backend/database"
 	"github.com/kerti/balances/backend/model"
 	"github.com/kerti/balances/backend/util/failure"
 	"github.com/stretchr/testify/assert"
-)
-
-var (
-	db   database.MySQL
-	mock sqlmock.Sqlmock
 )
 
 var (
@@ -45,15 +38,6 @@ var (
 		CreatedBy: testUserID,
 	}
 )
-
-func getMockedDriver(matcher sqlmock.QueryMatcher) (database.MySQL, sqlmock.Sqlmock) {
-	mockDB, sqlmock, _ := sqlmock.New(sqlmock.QueryMatcherOption(matcher))
-	mockSqlxDB := sqlx.NewDb(mockDB, "sqlmock")
-
-	mock = sqlmock
-	db.DB = mockSqlxDB
-	return db, mock
-}
 
 func TestUserRepository(t *testing.T) {
 
