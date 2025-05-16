c91b81bda3b9b78833c627792f8e4d338561137e
Raditya Kertiyasa <adit.kerti@gmail.com>
2025-05-16 09:32:15 +0700

    Add bank account details page and simplify route names.

--- frontend/vue/src/configs/routes.js
+++ frontend/vue/src/configs/routes.js
@@ -8,24 +8,25 @@ const routes = [
     // dashboard
     { path: '/dashboard', name: 'Dashboard', component: Dashboard },
     // assets
-    { path: '/assets/bank-accounts', name: 'Bank Accounts', component: () => import('@/pages/assets/BankAccounts.vue') },
-    { path: '/assets/vehicles', name: 'Vehicles', component: () => import('@/pages/assets/Vehicles.vue') },
-    { path: '/assets/properties', name: 'Properties', component: () => import('@/pages/assets/Properties.vue') },
+    { path: '/assets/bank-accounts', name: 'assets.bankaccounts', component: () => import('@/pages/assets/BankAccounts.vue') },
+    { path: '/assets/bank-accounts/:id', name: 'assets.bankaccount.detail', component: () => import('@/pages/assets/BankAccountDetail.vue') },
+    { path: '/assets/vehicles', name: 'assets.vehicles', component: () => import('@/pages/assets/Vehicles.vue') },
+    { path: '/assets/properties', name: 'assets.properties', component: () => import('@/pages/assets/Properties.vue') },
     // liabilities
-    { path: '/liabilities/personal-debts', name: 'Personal Debts', component: () => import('@/pages/liabilities/PersonalDebts.vue') },
-    { path: '/liabilities/institutional-debts', name: 'Institutional Debts', component: () => import('@/pages/liabilities/InstitutionalDebts.vue') },
+    { path: '/liabilities/personal-debts', name: 'liabilities.personal', component: () => import('@/pages/liabilities/PersonalDebts.vue') },
+    { path: '/liabilities/institutional-debts', name: 'liabilities.institutional', component: () => import('@/pages/liabilities/InstitutionalDebts.vue') },
     // investments
-    { path: '/investments/deposits', name: 'Deposits', component: () => import('@/pages/investments/Deposits.vue') },
-    { path: '/investments/obligations', name: 'Obligations', component: () => import('@/pages/investments/Obligations.vue') },
-    { path: '/investments/gold', name: 'Gold', component: () => import('@/pages/investments/Gold.vue') },
-    { path: '/investments/mutual-funds', name: 'Mutual Funds', component: () => import('@/pages/investments/MutualFunds.vue') },
-    { path: '/investments/stocks', name: 'Stocks', component: () => import('@/pages/investments/Stocks.vue') },
-    { path: '/investments/p2p-lendings', name: 'P2P Lendings', component: () => import('@/pages/investments/P2PLendings.vue') },
+    { path: '/investments/deposits', name: 'investments.deposits', component: () => import('@/pages/investments/Deposits.vue') },
+    { path: '/investments/obligations', name: 'investments.obligations', component: () => import('@/pages/investments/Obligations.vue') },
+    { path: '/investments/gold', name: 'investments.gold', component: () => import('@/pages/investments/Gold.vue') },
+    { path: '/investments/mutual-funds', name: 'investments.mutualfunds', component: () => import('@/pages/investments/MutualFunds.vue') },
+    { path: '/investments/stocks', name: 'investments.stocks', component: () => import('@/pages/investments/Stocks.vue') },
+    { path: '/investments/p2p-lendings', name: 'ivenstments.p2plendings', component: () => import('@/pages/investments/P2PLendings.vue') },
     // authentication and authorization
-    { path: '/login', name: 'Login', component: Login },
+    { path: '/login', name: 'login', component: Login },
     // miscellaneous
-    { path: '/about', name: 'About', component: () => import('@/pages/About.vue') },
-    { path: '/docs', name: 'Documentation', component: () => import('@/pages/Docs.vue') },
+    { path: '/about', name: 'misc.about', component: () => import('@/pages/About.vue') },
+    { path: '/docs', name: 'misc.docs', component: () => import('@/pages/Docs.vue') },
 ]
 
 const router = createRouter({
---
+++ frontend/vue/src/pages/assets/BankAccountDetail.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  This will be the Bank Account Detail page for ID {{ $route.params.id }}.
+</template>
