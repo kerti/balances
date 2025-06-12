ef9273d24c38b0db20a51fe53334a5ab48b08ad8
Raditya Kertiyasa <adit.kerti@gmail.com>
2025-06-12 18:11:12 +0700

    Generate mocks and initiate bank accounts service tests.

---
+++ backend/repository/repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"github.com/google/uuid"
+	"github.com/kerti/balances/backend/model"
+	"github.com/kerti/balances/backend/util/filter"
+)
+
+// BankAccount is the Bank Account repository interface
+type BankAccount interface {
+	Startup()
+	Shutdown()
+	ExistsByID(id uuid.UUID) (exists bool, err error)
+	ExistsBalanceByID(id uuid.UUID) (exists bool, err error)
+	ResolveByIDs(ids []uuid.UUID) (bankAccounts []model.BankAccount, err error)
+	ResolveBalancesByIDs(ids []uuid.UUID) (bankAccountBalances []model.BankAccountBalance, err error)
+	ResolveByFilter(filter filter.Filter) (bankAccounts []model.BankAccount, pageInfo model.PageInfoOutput, err error)
+	ResolveBalancesByFilter(filter filter.Filter) (bankAccountBalances []model.BankAccountBalance, pageInfo model.PageInfoOutput, err error)
+	ResolveLastBalancesByBankAccountID(id uuid.UUID, count int) (bankAccountBalances []model.BankAccountBalance, err error)
+	Create(bankAccount model.BankAccount) error
+	Update(bankAccount model.BankAccount) error
+	CreateBalance(bankAccountBalance model.BankAccountBalance, bankAccount *model.BankAccount) error
+	UpdateBalance(bankAccountBalance model.BankAccountBalance, bankAccount *model.BankAccount) error
+}
+
+// User is the User repository interface
+type User interface {
+	Startup()
+	Shutdown()
+	ExistsByID(id uuid.UUID) (exists bool, err error)
+	ResolveByIDs(ids []uuid.UUID) (users []model.User, err error)
+	ResolveByIdentity(identity string) (user model.User, err error)
+	ResolveByFilter(filter filter.Filter) (users []model.User, pageInfo model.PageInfoOutput, err error)
+	Create(user model.User) error
+	Update(user model.User) error
+}
---
+++ backend/service/banks_test.go
@@ -0,0 +1,57 @@
+package service_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	mock_repository "github.com/kerti/balances/backend/mock"
+	"github.com/kerti/balances/backend/model"
+	"github.com/kerti/balances/backend/service"
+	"github.com/kerti/balances/backend/util/cachetime"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type bankAccountsServiceTestSuite struct {
+	suite.Suite
+	ctrl     *gomock.Controller
+	svc      service.BankAccount
+	mockRepo *mock_repository.MockBankAccount
+}
+
+func TestBankAccountsService(t *testing.T) {
+	suite.Run(t, new(bankAccountsServiceTestSuite))
+}
+
+func (t *bankAccountsServiceTestSuite) SetupTest() {
+	t.ctrl = gomock.NewController(t.T())
+	t.mockRepo = mock_repository.NewMockBankAccount(t.ctrl)
+	t.svc = &service.BankAccountImpl{
+		Repository: t.mockRepo,
+	}
+}
+
+func (t *bankAccountsServiceTestSuite) TearDownTest() {
+	t.ctrl.Finish()
+}
+
+func (t *bankAccountsServiceTestSuite) TestResolveByID() {
+
+	t.Run("Exists", func() {
+		testID, _ := uuid.NewV7()
+		nilCacheTime := cachetime.NCacheTime{}
+		bankAccounts := []model.BankAccount{
+			{
+				ID: testID,
+			},
+		}
+
+		t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).Return(bankAccounts, nil)
+
+		_, err := t.svc.GetByID(testID, false, nilCacheTime, nilCacheTime, nil)
+
+		assert.NoError(t.T(), err, "should not error")
+	})
+
+}
