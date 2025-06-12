88a3dc41261492d335ad24e323c5b121789fc895
Raditya Kertiyasa <adit.kerti@gmail.com>
2025-06-12 22:26:31 +0700

    Add some more tests for resolving bank accounts by ID.

--- backend/service/banks_test.go
+++ backend/service/banks_test.go
@@ -2,9 +2,11 @@ package service_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
+	"github.com/guregu/null"
 	mock_repository "github.com/kerti/balances/backend/mock"
 	"github.com/kerti/balances/backend/model"
 	"github.com/kerti/balances/backend/service"
@@ -38,52 +40,135 @@ func (t *bankAccountsServiceTestSuite) TearDownTest() {
 
 func (t *bankAccountsServiceTestSuite) TestResolveByID() {
 
-	t.Run("TestExistsNoBalance", func() {
-		testID, _ := uuid.NewV7()
-		nilCacheTime := cachetime.NCacheTime{}
-		bankAccounts := []model.BankAccount{
-			{
-				ID: testID,
-			},
-		}
+	testID, _ := uuid.NewV7()
+	testBalanceID1, _ := uuid.NewV7()
+	testBalanceID2, _ := uuid.NewV7()
+	nilCacheTime := cachetime.NCacheTime{}
 
-		t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).Return(bankAccounts, nil)
+	resolvedBankAccount := model.BankAccount{
+		ID: testID,
+	}
 
-		_, err := t.svc.GetByID(testID, false, nilCacheTime, nilCacheTime, nil)
+	resolvedBankAccountSlice := []model.BankAccount{resolvedBankAccount}
 
-		assert.NoError(t.T(), err, "should not error")
-	})
+	resolvedBankAccountBalance1 := model.BankAccountBalance{
+		ID: testBalanceID1,
+	}
 
-	t.Run("TestExistsWithBalance", func() {
-		testID, _ := uuid.NewV7()
-		testBalanceID1, _ := uuid.NewV7()
-		testBalanceID2, _ := uuid.NewV7()
-		nilCacheTime := cachetime.NCacheTime{}
+	resolvedBankAccountBalance2 := model.BankAccountBalance{
+		ID: testBalanceID2,
+	}
 
-		bankAccounts := []model.BankAccount{
-			{
-				ID: testID,
-			},
-		}
-		bankAccountBalances := []model.BankAccountBalance{
-			{
-				ID: testBalanceID1,
-			},
-			{
-				ID: testBalanceID2,
-			},
-		}
-		balanceFilterInput := model.BankAccountBalanceFilterInput{
-			BankAccountIDs: &[]uuid.UUID{testID},
-		}
-		pageInfo := model.PageInfoOutput{}
+	resolvedBankAccountBalanceSlice := []model.BankAccountBalance{
+		resolvedBankAccountBalance1,
+		resolvedBankAccountBalance2,
+	}
 
-		t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).Return(bankAccounts, nil)
-		t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).Return(bankAccountBalances, pageInfo, nil)
+	t.Run("Exists", func() {
 
-		_, err := t.svc.GetByID(testID, true, nilCacheTime, nilCacheTime, nil)
+		t.Run("NoBalance", func() {
+			t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+				Return(resolvedBankAccountSlice, nil)
 
-		assert.NoError(t.T(), err, "should not error")
+			_, err := t.svc.GetByID(testID, false, nilCacheTime, nilCacheTime, nil)
+
+			assert.NoError(t.T(), err, "should not error")
+		})
+
+		t.Run("WithBalance", func() {
+
+			t.Run("NoFilter", func() {
+				balanceFilterInput := model.BankAccountBalanceFilterInput{
+					BankAccountIDs: &[]uuid.UUID{testID},
+				}
+				pageInfo := model.PageInfoOutput{}
+
+				t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+					Return(resolvedBankAccountSlice, nil)
+				t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).
+					Return(resolvedBankAccountBalanceSlice, pageInfo, nil)
+
+				_, err := t.svc.GetByID(testID, true, nilCacheTime, nilCacheTime, nil)
+
+				assert.NoError(t.T(), err, "should not error")
+			})
+
+			t.Run("WithStartDate", func() {
+				yesterday := cachetime.NCacheTime(null.TimeFrom(time.Now().AddDate(0, 0, -1)))
+				balanceFilterInput := model.BankAccountBalanceFilterInput{
+					BankAccountIDs: &[]uuid.UUID{testID},
+					StartDate:      yesterday,
+				}
+				pageInfo := model.PageInfoOutput{}
+
+				t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+					Return(resolvedBankAccountSlice, nil)
+				t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).
+					Return(resolvedBankAccountBalanceSlice, pageInfo, nil)
+
+				_, err := t.svc.GetByID(testID, true, yesterday, nilCacheTime, nil)
+
+				assert.NoError(t.T(), err, "should not error")
+			})
+
+			t.Run("WithEndDate", func() {
+				now := cachetime.NCacheTime(null.TimeFrom(time.Now()))
+				balanceFilterInput := model.BankAccountBalanceFilterInput{
+					BankAccountIDs: &[]uuid.UUID{testID},
+					EndDate:        now,
+				}
+				pageInfo := model.PageInfoOutput{}
+
+				t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+					Return(resolvedBankAccountSlice, nil)
+				t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).
+					Return(resolvedBankAccountBalanceSlice, pageInfo, nil)
+
+				_, err := t.svc.GetByID(testID, true, nilCacheTime, now, nil)
+
+				assert.NoError(t.T(), err, "should not error")
+			})
+
+			t.Run("WithBothDates", func() {
+				yesterday := cachetime.NCacheTime(null.TimeFrom(time.Now().AddDate(0, 0, -1)))
+				now := cachetime.NCacheTime(null.TimeFrom(time.Now()))
+				balanceFilterInput := model.BankAccountBalanceFilterInput{
+					BankAccountIDs: &[]uuid.UUID{testID},
+					StartDate:      yesterday,
+					EndDate:        now,
+				}
+				pageInfo := model.PageInfoOutput{}
+
+				t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+					Return(resolvedBankAccountSlice, nil)
+				t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).
+					Return(resolvedBankAccountBalanceSlice, pageInfo, nil)
+
+				_, err := t.svc.GetByID(testID, true, yesterday, now, nil)
+
+				assert.NoError(t.T(), err, "should not error")
+			})
+
+			t.Run("WithPageSize", func() {
+				pageSize := 120
+				balanceFilterInput := model.BankAccountBalanceFilterInput{
+					BankAccountIDs: &[]uuid.UUID{testID},
+				}
+				balanceFilterInput.PageSize = &pageSize
+				pageInfo := model.PageInfoOutput{}
+
+				t.mockRepo.EXPECT().ResolveByIDs([]uuid.UUID{testID}).
+					Return(resolvedBankAccountSlice, nil)
+				t.mockRepo.EXPECT().ResolveBalancesByFilter(balanceFilterInput.ToFilter()).
+					Return(resolvedBankAccountBalanceSlice, pageInfo, nil)
+
+				_, err := t.svc.GetByID(testID, true, nilCacheTime, nilCacheTime, &pageSize)
+
+				assert.NoError(t.T(), err, "should not error")
+			})
+
+		})
+
 	})
 
 }
